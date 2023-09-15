1483864bd7eea88bbce17d30d842025915e19f5b
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-15 16:54:53 +0530

    moved all difficulty.jsx code to App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
-import Difficulty from './Difficulty'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div>
-      <Difficulty />
-    </div>
-  )
+  const [gamemode, setGamemode] = useState("easy")
+  const [startGame, setStartGame] = useState(false)
+
+  if (startGame) {
+    return (
+      <div>
+        <h1>Game Starting</h1>
+        <h2>Playing on {gamemode} difficulty</h2>
+      </div>
+    )
+  }
+
+  else {
+    return (
+      <dialog open>
+        <h2>Choose Difficulty</h2>
+        <button onClick={() => {setGamemode("easy")}}>Easy</button>
+        <button onClick={() => {setGamemode("medium")}}>Medium</button>
+        <button onClick={() => {setGamemode("hard")}}>Hard</button>
+        <h3>Difficulty has been set to {gamemode}</h3>
+        <button onClick={() => {setStartGame(true)}}>Play</button>
+      </dialog>   
+    )
+  }
 }
 
 export default App
