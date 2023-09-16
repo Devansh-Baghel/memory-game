27b4442a0c78f9dbd03a0351eebff33526da6584
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-16 15:13:42 +0530

    displays 6 random flags on easy, 10 random on medium, 14 random on hard

--- src/components/PlayGame.jsx
+++ src/components/PlayGame.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 function PlayGame(props) {
-
   const countries = [
     "kr",
     "se",
@@ -16,8 +15,8 @@ function PlayGame(props) {
     "ru",
     "au",
     "it",
-    "in"
-  ]
+    "in",
+  ];
 
   const shuffle = (array) => {
     return array.sort(() => Math.random() - 0.5);
@@ -25,38 +24,62 @@ function PlayGame(props) {
 
   // 1. Pick 6,10,14 number of countries from the list above
   // okkay new plan, not gonna get the flags from an api.
-  
+
   const randomCountries = shuffle(countries);
-  console.log(randomCountries)
+  console.log(randomCountries);
 
-  // if (props.gamemode === "easy") {
-  //   getCards(6);
-  // } else if (props.gamemode === "medium") {
-  //   getCards(10);
-  // } else if (props.gamemode === "hard") {
-  //   getCards(14);
-  // }
-
-  return (
-    <div>
-      <h1>Game Starting</h1>
-      <h2>Playing on {props.gamemode} difficulty</h2>
-      <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[6]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[7]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[8]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[9]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[10]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[11]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[12]}.png`} />
-      <img src={`https://flagcdn.com/w320/${randomCountries[13]}.png`} />
-    </div>
-  );
+  if (props.gamemode === "easy") {
+    return (
+      <div>
+        <h1>Game Starting</h1>
+        <h2>Playing on {props.gamemode} difficulty</h2>
+        <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
+      </div>
+    );
+  } else if (props.gamemode === "medium") {
+    return (
+      <div>
+        <h1>Game Starting</h1>
+        <h2>Playing on {props.gamemode} difficulty</h2>
+        <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[6]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[7]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[8]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[9]}.png`} />
+      </div>
+    );
+  } else if (props.gamemode === "hard") {
+    return (
+      <div>
+        <h1>Game Starting</h1>
+        <h2>Playing on {props.gamemode} difficulty</h2>
+        <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[6]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[7]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[8]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[9]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[10]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[11]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[12]}.png`} />
+        <img src={`https://flagcdn.com/w320/${randomCountries[13]}.png`} />
+      </div>
+    );
+  }
 }
 
 export default PlayGame;
