273a0065242a1e85bcf7b0167659d764b60b3105
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-16 15:09:50 +0530

    generates 14 flags in random order

--- src/components/PlayGame.jsx
+++ src/components/PlayGame.jsx
@@ -1,54 +1,60 @@
 import React from "react";
-import axios from "axios";
 
 function PlayGame(props) {
+
   const countries = [
-    "Republic of india",
-    "australia",
-    "brazil",
-    "japan",
-    "china",
-    "sweden",
-    "south africa",
-    "canada",
-    "france",
-    "indonesia",
-    "denmark",
-    "italy",
-    "russia",
-    "bhutan",
-  ];
+    "kr",
+    "se",
+    "id",
+    "ca",
+    "za",
+    "br",
+    "jp",
+    "dk",
+    "bt",
+    "fr",
+    "ru",
+    "au",
+    "it",
+    "in"
+  ]
 
   const shuffle = (array) => {
     return array.sort(() => Math.random() - 0.5);
   };
 
-  function getCards(number) {
-    // 1. Pick 6,10,14 number of countries from the list above
-    // 2. Make that many api calls and get flags of them stored up
-
-    const shuffledCountries = shuffle(countries);
-    axios
-      .get(`https://restcountries.com/v3.1/name/${shuffledCountries[0]}`)
-      .then((res) => {
-        console.log(res.data[0].flags.png);
-      });
-  }
+  // 1. Pick 6,10,14 number of countries from the list above
+  // okkay new plan, not gonna get the flags from an api.
   
-  getCards(12);
+  const randomCountries = shuffle(countries);
+  console.log(randomCountries)
 
-  if (props.gamemode === "easy") {
-    getCards(6);
-  } else if (props.gamemode === "medium") {
-    getCards(10);
-  } else if (props.gamemode === "hard") {
-    getCards(14);
-  }
+  // if (props.gamemode === "easy") {
+  //   getCards(6);
+  // } else if (props.gamemode === "medium") {
+  //   getCards(10);
+  // } else if (props.gamemode === "hard") {
+  //   getCards(14);
+  // }
 
   return (
     <div>
       <h1>Game Starting</h1>
       <h2>Playing on {props.gamemode} difficulty</h2>
+      <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[6]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[7]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[8]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[9]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[10]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[11]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[12]}.png`} />
+      <img src={`https://flagcdn.com/w320/${randomCountries[13]}.png`} />
     </div>
   );
 }
--- src/components/example.js
+++ src/components/example.js
@@ -1,20 +1,20 @@
 import axios from "axios";
 
 const countries = [
-  "Republic of india",
-  "australia",
-  "brazil",
-  "japan",
-  "china",
-  "sweden",
-  "south africa",
-  "canada",
-  "france",
-  "indonesia",
-  "denmark",
-  "italy",
-  "russia",
-  "bhutan",
+  // "Republic of india",
+  // "australia",
+  // "brazil",
+  // "japan",
+  "South korea",
+  // "sweden",
+  // "south africa",
+  // "canada",
+  // "france",
+  // "indonesia",
+  // "denmark",
+  // "italy",
+  // "russia",
+  // "bhutan",
 ];
 
 
@@ -27,3 +27,20 @@ axios.get(`https://restcountries.com/v3.1/name/${shuffledCountries[0]}`).then(re
   console.log(res.data[0].flags.png)
   console.log(res.data[0].name.common)
 })
+
+const codes = [
+  "kr",
+  "se",
+  "id",
+  "ca",
+  "za",
+  "br",
+  "jp",
+  "dk",
+  "bt",
+  "fr",
+  "ru",
+  "au",
+  "it",
+  "in"
+]
