29de8da2a8bcd4edb2779d66b13550f911358ec1
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-16 17:03:39 +0530

    flag randomization work as intended also reduced code duplication

--- src/components/PlayGame.jsx
+++ src/components/PlayGame.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import { useEffect } from "react";
 
 function PlayGame(props) {
   const countries = [
@@ -19,76 +20,41 @@ function PlayGame(props) {
   ];
 
   const obj = {
-    easy: countries.slice(0,6),
-    medium: countries.slice(0,10),
+    easy: countries.slice(0, 6),
+    medium: countries.slice(0, 10),
     hard: countries,
-  }
+  };
 
   const shuffle = (array) => {
     return array.sort(() => Math.random() - 0.5);
   };
 
-  if (props.gamemode === "easy") {
-    const randomCountries = shuffle(obj.easy);
+  const [randomCountries, setRandomCountries] = useState([]);
 
-    return (
-      <div>
-        <h1>Game Starting</h1>
-        <h2>Playing on {props.gamemode} difficulty</h2>
-        <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
-        {/* <button onClick={() => {setRandomCountries(shuffle(countries))}}>Randomize</button> */}
-      </div>
-    );
-  } else if (props.gamemode === "medium") {
-    const randomCountries = shuffle(obj.medium);
+  useEffect(() => {
+    const shuffledArray = shuffle(obj[props.gamemode]);
+    setRandomCountries(shuffledArray);
+  }, [props.gamemode]);
 
-    return (
-      <div>
-        <h1>Game Starting</h1>
-        <h2>Playing on {props.gamemode} difficulty</h2>
-        <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[6]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[7]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[8]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[9]}.png`} />
-        {/* <button onClick={() => {setRandomCountries(shuffle(countries))}}>Randomize</button> */}
-      </div>
-    );
-  } else if (props.gamemode === "hard") {
-    const randomCountries = shuffle(obj.hard);
+  const handleRandomizeClick = () => {
+    const shuffledArray = shuffle(obj[props.gamemode]);
+    setRandomCountries(shuffledArray);
+  };
 
-    return (
-      <div>
-        <h1>Game Starting</h1>
-        <h2>Playing on {props.gamemode} difficulty</h2>
-        <img src={`https://flagcdn.com/w320/${randomCountries[0]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[1]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[2]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[3]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[4]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[5]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[6]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[7]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[8]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[9]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[10]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[11]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[12]}.png`} />
-        <img src={`https://flagcdn.com/w320/${randomCountries[13]}.png`} />
-        {/* <button onClick={() => {setRandomCountries(shuffle(countries))}}>Randomize</button> */}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Game Starting</h1>
+      <h2>Playing on {props.gamemode} difficulty</h2>
+      {randomCountries.map((country, index) => (
+        <img
+          key={index}
+          src={`https://flagcdn.com/w320/${country}.png`}
+          alt={country}
+        />
+      ))}
+      <button onClick={handleRandomizeClick}>Randomize</button>
+    </div>
+  );
 }
 
 export default PlayGame;
