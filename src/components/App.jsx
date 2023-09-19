f44e1e78229c1ed576881b90ab775719419f3f72
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-19 21:22:06 +0530

    Added Scss

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import PlayGame from './PlayGame'
+import "../styles/App.scss"
 
 function App() {
   const [gamemode, setGamemode] = useState("easy")
