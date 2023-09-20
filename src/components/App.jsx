8910c6f5fb59f85f5d43b03cf357634c8d95e0ec
Devansh_Baghel <devanshbaghel85@gmail.com>
2023-09-20 16:00:58 +0530

    updated set difficulty screen

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,30 +1,45 @@
-import React from 'react'
-import { useState } from 'react'
-import PlayGame from './PlayGame'
-import "../styles/App.scss"
+import React from "react";
+import { useState } from "react";
+import PlayGame from "./PlayGame";
+import "../styles/App.scss";
 
 function App() {
-  const [gamemode, setGamemode] = useState("easy")
-  const [startGame, setStartGame] = useState(false)
+  const [gamemode, setGamemode] = useState("easy");
+  const [startGame, setStartGame] = useState(false);
 
   if (startGame) {
-    return (
-      <PlayGame gamemode={gamemode} />
-    )
-  }
-
-  else {
+    return <PlayGame gamemode={gamemode} />;
+  } else {
     return (
       <dialog open>
         <h2>Choose Difficulty</h2>
-        <button onClick={() => {setGamemode("easy")}}>Easy</button>
-        <button onClick={() => {setGamemode("medium")}}>Medium</button>
-        <button onClick={() => {setGamemode("hard")}}>Hard</button>
-        <h3>Difficulty has been set to {gamemode}</h3>
-        <button onClick={() => {setStartGame(true)}}>Play</button>
+        <button
+          onClick={() => {
+            setGamemode("easy");
+            setStartGame(true)
+          }}
+        >
+          Easy
+        </button>
+        <button
+          onClick={() => {
+            setGamemode("medium");
+            setStartGame(true)
+          }}
+        >
+          Medium
+        </button>
+        <button
+          onClick={() => {
+            setGamemode("hard");
+            setStartGame(true)
+          }}
+        >
+          Hard
+        </button>
       </dialog>
-    )
+    );
   }
 }
 
-export default App
+export default App;
